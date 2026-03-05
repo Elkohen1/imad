82957964fa6e114a9a7b8838ca706e50730750d6
Elkohen1 <162137147+Elkohen1@users.noreply.github.com>
2026-03-05 15:37:48 -0500

    Add files via upload

---
+++ projet1.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+let sliders = document.querySelectorAll(".slider");
+
+sliders.forEach(slider => {
+
+let slides = slider.querySelectorAll(".slide");
+
+let index = 0;
+
+setInterval(() => {
+
+slides[index].classList.remove("active");
+
+index = (index + 1) % slides.length;
+
+slides[index].classList.add("active");
+
+}, 3000);
+
+});
+
+});
